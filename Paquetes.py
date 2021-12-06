b40c267a746a9901be62c89bb6b1f3a30dda7c0f
Jazmin <portillojsn@gmail.com>
2021-12-05 20:16:56 -0600

    Pruebas

--- Paquetes.py
+++ Paquetes.py
@@ -21,7 +21,7 @@ n = 1 #Indice de nodos
 W = [16,32,64,128,256] #Miniranuras
 w = 1 #Indice de miniranuras
 landa = [0.0005,0.001,0.005,0.03] #Tasa de generación de paquetes
-l = 0 #Indice de landa
+l = 0#Indice de landa
 
 #Tiempo de slot
 Tslot = sigma*W[w] + DIFS + 3*SIFS + durRTS + durCTS + durDATA + durACK 
@@ -54,12 +54,15 @@ final = 0
 counter = 0
 
 Pa = np.zeros((1,5))
-contendientes = [0] * N[n]
+contendientes = [0] * N[n] #nodos con el menor número de backoff
 numPaquete = 0
+x = 0
+y = 0
+z = 0
 
 #Generacion de paquetes
 for i in range (1,1330000 + 1):
-    if ta <= tsim:
+    if ta < tsim:
         landa2 = landa[l]*N[n]*H
         U = (1e6*random())/1e6
         nuevot = -(1/landa2)*log10(1-U)
@@ -78,17 +81,11 @@ for i in range (1,1330000 + 1):
                 hashMap[llave] = aux
                 aux = [0] * K           
     
-            Pa[pg-1][0] = pg
-            Pa[pg-1][1] = nodo
-            Pa[pg-1][2] = grado
-            Pa[pg-1][3] = ta
-            Pa = np.vstack([Pa,np.zeros((1,5))])  
-    
         ta = tsim + nuevot
 
     if Decimal(str(i))%Decimal(str(Tcycle)) == 0:
         #Transmisión, recorremos desde nodo más alejado
-        for grado in range(H-1,0,-1):
+        for grado in range(H-1,-1,-1):
             for nodo in range(0,N[n]):
                 if(Nodos[nodo,grado] != 0):
                     contendientes[nodo] = randint(0,W[n]-1)
@@ -97,7 +94,7 @@ for i in range (1,1330000 + 1):
             #Colisiones
             #El contador más pequeño es el que transmite
             nodoGanador = min(filter(lambda x: x is not None, contendientes)) if any(contendientes) else None
-            colisiones = [indice for indice, item in enumerate(contendientes) if item == nodoGanador]
+            colisiones = [indice for indice, item in enumerate(contendientes) if item == nodoGanador]#busaca que contendientes tienen el mismo número de contador
 
             if len(colisiones) > 1:
                 #Hay una colisión
@@ -131,17 +128,18 @@ for i in range (1,1330000 + 1):
                     hashMap[llaveHash2] = aux2
                     Nodos[indexNodo[0], grado] = Nodos[indexNodo[0], grado] - 1 #Actualizar matriz Nodos (grado I)
 
-                    if grado - 1 != 0: #Sumar al buffer del hashMap paquete del nodo (Grado I -1)
+                    if grado - 1 != -1: #Sumar al buffer del hashMap paquete del nodo (Grado I -1)
                         llaveHash3 = "{}{}".format(indexNodo[0],grado - 1)
                         aux3 =  hashMap[llaveHash3]
                         a = [indice for indice, item1 in enumerate(aux3) if item1 == 0]
-                        #print(a)
+    
                         if a:
                             aux3[a[0]] = numPaquete
                             transpergrade[grado] = transpergrade[grado] + 1
                             hashMap[llaveHash3] =  aux3
                             Nodos[indexNodo, grado - 1] = Nodos[indexNodo, grado -1] + 1 #Actualizar matriz Nodos (grado I-1)
                         else:
+                            x += 1
                             pass
                     
                     else:
@@ -151,4 +149,3 @@ for i in range (1,1330000 + 1):
 
         #Termina Transmitir cada ciclo de trabajo.
     tsim = tsim + Tcycle
-#print(Pa)
